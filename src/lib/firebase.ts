3ece7db49100790a07f02e37a0574f2bd7bc65fe
robertomejia21 <yo@robertomejiab.com>
2026-02-19 18:30:03 -0800

    fix(firebase): add fallback for storage bucket to prevent upload fail

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,7 @@ const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "north3-8274b.firebasestorage.app",
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
